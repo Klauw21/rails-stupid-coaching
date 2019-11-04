f1ea92d5d4929b97e4db7fa8298e5a1fdf264664
Claudia van Tongeren <klauw21@gmail.com>
2019-11-04 17:13:14 +0100

    didn't finish the entire challenge, but until this point i think i really understand it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true`.
 Rails.application.routes.draw do
+  get 'pages/questions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ask GET  /ask(.:format)    questions#ask
+  # answer GET  /answer(.:format) questions#answer
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
 end
